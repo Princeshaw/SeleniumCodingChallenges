57c7590383f445f9add5ae1785a0f84fb5dc1195
Prince Shaw <prince54shaw@gmail.com>
2021-04-15 02:04:07 +0530

    Added selenium tasks

---
+++ SeleniumCodingChallenges/Challenges/CarouselHandling.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace SeleniumCodingChallenges.Challenges
+{
+    class CarouselHandling
+    {
+        protected IWebDriver driver;
+        [SetUp]
+        public void init()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.noon.com/uae-en/");
+
+        }
+        [TearDown]
+        public void Close()
+        {
+            driver.Close();
+        }
+
+
+        public void click(By locator)
+        {
+            try
+            {
+                driver.FindElement(locator).Click();
+            }
+            catch
+            {
+                try {
+                    IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+
+                    jse.ExecuteScript("arguments[0].scrollIntoView()", driver.FindElement(locator));
+                }
+                catch(Exception e){
+                    throw (e);
+                }
+            }
+        }
+
+
+        [Test]
+        public void test() {
+
+           int elementsCount = driver.FindElements(By.XPath("(//div[@class='sc-GTWni GkeGT'])[1]//div[@data-qa='product-name']//div")).Count;
+
+            for(int i=1;i<=elementsCount;i++)
+            {
+                IWebElement e = driver.FindElement(By.XPath("((//div[@class='sc-GTWni GkeGT'])[1]//div[@data-qa='product-name']//div)["+i+"]"));
+                bool isDisplayed = e.Displayed;
+                if (!isDisplayed) {
+                    click(By.XPath("(//div[@class='sc-GTWni GkeGT'])[1]//div[contains(@class,'swiper-button-next custom-navigation')]"));
+                    e = driver.FindElement(By.XPath("((//div[@class='sc-GTWni GkeGT'])[1]//div[@data-qa='product-name']//div)[" + i + "]"));
+
+                }
+                Console.WriteLine(i+ " : "+ e.Text);
+            }
+
+        
+        }
+    }
+}
---
+++ SeleniumCodingChallenges/Challenges/WorldPopulationCounts.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumCodingChallenges.Challenges
+{
+    class WorldPopulationCounts
+    {
+        protected IWebDriver driver;
+        [SetUp]
+        public void init()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.worldometers.info/world-population/");
+
+        }
+        [TearDown]
+        public void Close()
+        {
+            driver.Close();
+        }
+
+        public string GetPopulationCount(string locator)
+        {
+            return (driver.FindElement(By.XPath(locator)).Text);
+        }
+
+        [Test]
+        public void Test()
+        {
+
+            string currentCountXpath = "//div[@class='maincounter-number']//span[@class='rts-counter']";
+
+            string todayBirthsCountXpath = "//div[@class='sec-counter']//span[@rel='births_today']";
+
+            string todayDeathsCountXpath = "//div[@class='sec-counter']//span[@rel='dth1s_today']";
+
+            string todayGrowthCountXpath = "//div[@class='sec-counter']//span[@rel='absolute_growth']";
+
+            string thisYearBirthsCountXpath = "//div[@class='sec-counter']//span[@rel='births_this_year']";
+
+            string thisYearDeathsCountXpath = "//div[@class='sec-counter']//span[@rel='dth1s_this_year']";
+
+            string thisYearGrowthCountXpath = "//div[@class='sec-counter']//span[@rel='absolute_growth_year']";
+
+
+            int time = 1;
+            while (true)
+            {
+                if (time == 21)
+                    break;
+                string totalCurrentCount = GetPopulationCount(currentCountXpath);
+
+                string todayBirthsCount = GetPopulationCount(todayBirthsCountXpath);
+
+                string todayDeathsCount = GetPopulationCount(todayDeathsCountXpath);
+
+                string todayGrowthCount = GetPopulationCount(todayGrowthCountXpath);
+
+                string thisYearBirthsCount = GetPopulationCount(thisYearBirthsCountXpath);
+
+                string thisYearDeathsCount = GetPopulationCount(thisYearDeathsCountXpath);
+
+                string thisYearGrowthCount = GetPopulationCount(thisYearGrowthCountXpath);
+
+
+
+                Console.WriteLine(String.Format("Time {0} sec, \n Current World Population : {1} \n " +
+                    "Births today : {2} \t\t Births this year : {5}  \n " +
+                    "Deaths today : {3} \t\t Deaths this year : {6} \n " +
+                    "Population Growth today : {4} \t\t BPopulation Growth this year : {7} ", time, totalCurrentCount, todayBirthsCount, todayDeathsCount, todayGrowthCount, thisYearBirthsCount, thisYearDeathsCount, thisYearGrowthCount));
+
+                Thread.Sleep(1000);
+                time++;
+
+            }
+
+        }
+    }
+}
